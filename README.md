d2d20653bd24a0fe32137383b14fa333398a5df4
kovzol <zoltan@geogebra.org>
2019-06-24 18:09:40 +0200

    First version (that should work out of the box)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # buchberger-singular
-Explain Buchberger's algorithm by using Singular as a backend
+This project is about to explain Buchberger's algorithm by using Singular as a backend.
+
+## Installation
+1. Copy the files into a folder on your web site and make sure they are accessible via the web from a browser.
+2. Install PHP as module in your web server.
+3. Install the Singular computer algebra system.
+
+## Usage
+1. Point your web browser to the installation folder and start *index.html*.
+2. Fill in the forms and press the *Submit* button.
+3. Follow the results on the next page.
---
+++ action.php
@@ -0,0 +1,57 @@
+<html>
+    <script type="text/x-mathjax-config">
+        MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+    </script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' async></script>
+
+<?php
+
+ $vars = $_GET["vars"];
+ $polys = $_GET["polys"];
+ $ord = $_GET["ord"];
+ $hide = $_GET["hide"];
+ $terms = $_GET["terms"];
+
+ $polysA = explode("\n", $polys);
+
+ $code = 'LIB "teachstd.lib";' . "\n" . 'LIB "latex.lib";' . "\n";
+ $code .= 'ring r = 0, (' . $vars . '), ' . $ord . ';' . "\n";
+
+ $inputcode = 'list input = ';
+ for ($i = 0; $i < count($polysA); $i++) {
+     $code .= 'poly p' . ($i+1) . ' = ' . rtrim($polysA[$i]) . ';' . "\n";
+     if ($i > 0)
+         $inputcode .= ', ';
+     $inputcode .= 'p' . ($i+1);
+     }
+ $inputcode .= ';' . "\n";
+
+ $code .= $inputcode;
+ $code .= 'int hidechains = ';
+ if ($hide == 'yes')
+     $code .= '1';
+ else
+     $code .= '0';
+ $code .= ';' . "\n";
+
+ $code .= 'int TeXwidth = ';
+ $code .= $terms . ';' . "\n";
+
+ $code .= file_get_contents("buchberger2-singular.txt");
+
+ $tmp_singularcode_file = tmpfile();
+ $tmp_singularcode_path = stream_get_meta_data($tmp_singularcode_file)['uri'];
+ fwrite($tmp_singularcode_file, $code);
+ $commandline = "Singular -q -b < " . $tmp_singularcode_path . " 2>&1";
+ exec($commandline, $output, $retval);
+ fclose($tmp_singularcode_file);
+
+ echo "<body><div>\n";
+ for ($i=0; $i<count($output); $i++) {
+     echo $output[$i];
+     }
+ echo "</div></body>";
+ 
+?>
+
+</html>
